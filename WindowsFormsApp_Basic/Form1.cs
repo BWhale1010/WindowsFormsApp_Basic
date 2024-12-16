ca7efaa762fa7adb4277e54415dd56044039fa29
BWhale1010 <bawool1010@daum.net>
2024-12-16 13:34:23 +0900

    변수 및 캐스팅 실습

--- WindowsFormsApp_Basic/Form1.cs
+++ WindowsFormsApp_Basic/Form1.cs
@@ -15,6 +15,34 @@ namespace WindowsFormsApp_Basic
         public Form1()
         {
             InitializeComponent();
+
+            byte classCount = 10;
+            short employeeNum = 100;
+            int boardNum = 405004004;
+            float marathonDistance = 42.195f;
+            double apartmentWidth = 10.11d;
+            decimal worldPopulation = 7000000000;
+
+            textBox_print.Text += classCount.GetType()+" : ";
+            textBox_print.Text += classCount.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += employeeNum.GetType() + " : ";
+            textBox_print.Text += employeeNum.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += boardNum.GetType() + " : ";
+            textBox_print.Text += boardNum.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += marathonDistance.GetType() + " : ";
+            textBox_print.Text += marathonDistance.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += apartmentWidth.GetType() + " : ";
+            textBox_print.Text += apartmentWidth.ToString();
+            textBox_print.Text += "\r\n";
+            textBox_print.Text += worldPopulation.GetType() + " : ";
+            textBox_print.Text += worldPopulation.ToString();
+
+
+
         }
     }
 }
