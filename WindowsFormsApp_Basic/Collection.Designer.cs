b181e812998e5add7017e9fb51cfca207460df29
BWhale1010 <bawool1010@daum.net>
2025-01-05 19:47:49 +0900

    collection 실습

---
+++ WindowsFormsApp_Basic/Collection.Designer.cs
@@ -0,0 +1,135 @@
+﻿namespace WindowsFormsApp_Basic
+{
+    partial class Collection
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox_id = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // textBox_id
+            // 
+            this.textBox_id.Location = new System.Drawing.Point(184, 139);
+            this.textBox_id.Name = "textBox_id";
+            this.textBox_id.Size = new System.Drawing.Size(275, 25);
+            this.textBox_id.TabIndex = 0;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Malgun Gothic", 19.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label1.Location = new System.Drawing.Point(194, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(119, 45);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "로그인";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Gulim", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button1.Location = new System.Drawing.Point(202, 332);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(130, 45);
+            this.button1.TabIndex = 4;
+            this.button1.Text = "로그인";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Gulim", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label2.Location = new System.Drawing.Point(51, 141);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 23);
+            this.label2.TabIndex = 2;
+            this.label2.Text = "아이디";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Gulim", 13.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.label3.Location = new System.Drawing.Point(51, 210);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(106, 23);
+            this.label3.TabIndex = 5;
+            this.label3.Text = "비밀번호";
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(184, 210);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(275, 25);
+            this.textBox_password.TabIndex = 6;
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Gulim", 13.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(129)));
+            this.button2.Location = new System.Drawing.Point(42, 37);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(115, 45);
+            this.button2.TabIndex = 7;
+            this.button2.Text = "파일열기";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Collection
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(558, 450);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.textBox_password);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox_id);
+            this.Name = "Collection";
+            this.Text = "Collection";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox_id;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button2;
+    }
+}
---
+++ WindowsFormsApp_Basic/Collection.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Basic
+{
+    public partial class Collection : Form
+    {
+        List<string> list = new List<string>();
+        Dictionary<string, string> idPassDic = new Dictionary<string, string>();
+        Dictionary<string, string> idCallNumDic = new Dictionary<string, string>();
+
+        public Collection()
+        {
+            InitializeComponent();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                string path = openFileDialog.FileName;
+                Stream fileStream = openFileDialog.OpenFile();
+
+                using (StreamReader streamReader = new StreamReader(fileStream))
+                {
+                    string line;
+
+                    while ((line = streamReader.ReadLine()) != null)
+                    {
+                        list.Clear();
+
+                        string[] strVal = line.Split(',');
+                        foreach (string str in strVal)
+                        {
+                            list.Add(str);
+                        }
+
+                        idPassDic.Add(list[0], list[1]);
+                        if (list.Count() == 2)
+                        {
+                            idCallNumDic.Add(list[0], null);
+                        }
+                        else
+                        {
+                            idCallNumDic.Add(list[0], list[2]);
+                        }
+                    }
+                }
+            }
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string password = "";
+            string callNum = "";
+
+            if (idPassDic.ContainsKey(textBox_id.Text) && idPassDic.TryGetValue(textBox_id.Text, out password))
+            {
+                if (password.Equals(textBox_password.Text))
+                {
+                    idCallNumDic.TryGetValue(textBox_id.Text, out callNum);
+                    try
+                    {
+                        MessageBox.Show("아이디 : " + textBox_id.Text + "\r\n전화번호 : " + callNum.ToString());
+                    }
+                    catch(NullReferenceException) 
+                    {
+                        MessageBox.Show("아이디 : " + textBox_id.Text + "\r\n전화번호 : 없음");
+                    }
+                    //catch(Exception ex) 
+                    //{
+                    //    MessageBox.Show(ex.Message);
+                    //}
+                    
+                }
+            }
+            else
+            {
+                MessageBox.Show("아이디와 비밀번호가 틀렸습니다.");
+            }
+        }
+
+
+    }
+}
