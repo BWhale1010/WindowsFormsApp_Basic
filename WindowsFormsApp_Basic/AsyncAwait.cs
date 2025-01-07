5d064c4fd5b1a3ebae3563bfefe5a7efa4dd4109
BWhale1010 <bawool1010@daum.net>
2025-01-07 13:28:49 +0900

    task, async, await 실습

---
+++ WindowsFormsApp_Basic/AsyncAwait.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp_Basic
+{
+    public partial class AsyncAwait : Form
+    {
+        public AsyncAwait()
+        {
+            InitializeComponent();
+        }
+
+        private async void button1_Click(object sender, EventArgs e)
+        {
+            await ReadFileAsync();
+        }
+
+        async Task ReadFileAsync()
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog();
+            string fileName = "";
+            string result = "";
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                fileName = openFileDialog.FileName;
+
+                using (StreamReader streamReader = File.OpenText(fileName))
+                {
+                    result = await streamReader.ReadToEndAsync();
+                }
+
+                textBox1.Text = result;
+            }
+        }
+    }
+}
